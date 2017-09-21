91feeabd4cf71826189873fad68aee7c50e9da15
VictorRuizEst98 <31596271+VictorRuizEst98@users.noreply.github.com>
2017-09-20 20:12:36 -0500

    Tarea 8 carpeta

---
+++ Tarea 8/Cuadro de + vacio.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+int x;
+int i;
+int j;
+ printf("Inserta el lado del cuadrado:\n");
+ scanf("%d",&x);
+  for(i=1;i<=x;i++) {
+    if (i==1||i==x) 
+  for(j=1;j<=x;j++) {
+        printf("+"); }
+     else {{
+        printf("+"); }
+  for(j=1;j<=x-2;j++) 
+        printf(" ");
+         printf("+");}
+          printf("\n");}
+            printf("Fin del programa UwU");}
+
---
+++ Tarea 8/Tablas 3 al 6.c
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdio.h>
+
+int main (int argc, char* argv []) { 
+int uwu;
+int owo;
+int ewe; 
+int awa;
+int A;
+int B;
+int C;
+int D;
+int total=0;
+printf("Tablas del multiplicar del 3 al 6");
+printf("\nPresiona la tecla del numero 3");
+ scanf("%d",&A);
+  if(A!=3);
+else printf("Presiona la telca del numero 4");
+ scanf("%d,",&B);
+ if(B!=4);
+else printf("Presiona la tecla del numero 5");
+ scanf("%d",&C);
+ if(C!=5);
+else printf("Presiona la tecla del numero 6");
+ scanf("%d",&D);
+ if(D!=6);
+  else
+    for(uwu=0;uwu<10;uwu=uwu+1)
+    {printf("\n3 * %d = %d\n",uwu+1,3*(uwu+1));
+    for(owo=0;owo<=uwu;owo=uwu+1)
+    { printf("4 * %d = %d\n",uwu+1,4*(uwu+1));  
+    for(ewe=0;ewe<=uwu;ewe=uwu+1)
+	{printf("5 * %d = %d\n",uwu+1,5*(uwu+1));
+	 for(awa=0;awa<=uwu;awa=uwu+1);
+	{printf("6 * %d = %d\n",uwu+1,6*(uwu+1));}
+			}}}}    
+
+    
+
+
