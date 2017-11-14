e03b4dbd3186180f580abf7bbf0275537dda038d
VictorRuizEst98 <31596271+VictorRuizEst98@users.noreply.github.com>
2017-11-14 01:44:31 -0600

    Tarea 21

---
+++ Tarea 21/nombres blilbio.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void letrav(){
+ printf("\n\nV   V\nV   V\n V V\n V V\n  V");
+}
+void letrai(){
+ printf("\n\nIIII\nIIII\nIIII\nIIII\nIIII");
+}
+void letrac(){
+ printf("\n\nCCCC\nC\nC\nC\nCCCC");
+}
+void letrat(){
+ printf("\n\nTTTT\n TT\n TT\n TT\n TT");
+}
+void letrao(){
+ printf("\n\n OO\nO  O\nO  O\nO  O\n OO");
+}
+void letrar(){
+ printf("\n\nRRRR\nR  R\nRRRR\nR R\nR  R");
+}
+
---
+++ Tarea 21/nombres.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <nombres.h>
+
+
+int main(){
+
+printf("VICTOR");
+
+letrav();
+letrai();
+letrac();
+letrat();
+letrao();
+letrar();
+
+}
+
+ 
+
